7649b64980528ce6e0d46a3ba8a40d8fc5a7968e
amsang <sang-uk.lee@stonybrook.edu>
2018-08-02 17:02:28 +0900

    memory update

---
+++ Project/Assets/Main_Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Game : MonoBehaviour
+{
+    public static bool GAME_RUNNING;
+    public static bool USER_SUCCESS;
+    public static bool ONE_REMAINING = false;
+    public readonly int CORRECT_ANS = 1;
+    public readonly int WRONG_ANS = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GAME_RUNNING = true;
+        USER_SUCCESS = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GAME_RUNNING) {
+            if (true) {
+                
+                USER_SUCCESS = true;
+            }
+        }
+        GAME_RUNNING = false;
+	}
+}
---
+++ Project/Assets/Video_Renderer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video_Renderer : MonoBehaviour {
+
+    public void disable()
+    {
+        Destroy(this.gameObject);
+    }
+}
